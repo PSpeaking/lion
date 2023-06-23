14b489173fedd8e6f471b6e1efa4798d5d1888ef
PSpeaking <137413264+PSpeaking@users.noreply.github.com>
2023-06-23 12:28:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function checkAnswer() {
   var selectedAnswer = document.querySelector('input[name="answer"]:checked');
   var userAnswer = selectedAnswer ? selectedAnswer.value : '';
 
-  if (userAnswer === 'lion') {
+  if (userAnswer === 'a conformist in') {
     document.getElementById('feedback').innerHTML = 'Congratulations! Your answer is correct!';
   } else {
     document.getElementById('feedback').innerHTML = 'Oops! Your answer is incorrect. Please try again.';
